fefef4b26801bb4926ad54c0bf07052b7f8861d9
purple <purpletianjing@163.com>
2015-09-11 21:28:22 +0800

    add comtents to model/classify.js

--- model/classify.js
+++ model/classify.js
@@ -1,3 +1,22 @@
 function Classify() {
-    
+
 }
+
+Classify.prototype.group = function(bookIds) {
+  var groupResult = [];
+  var groupTemp = {};
+  var judgeResult;
+
+  book_ids.forEach(function(bookId) {
+    judgeExist(bookId,groupResult);
+  });
+  return groupResult;
+};
+
+function judgeExist(bookId,groupResult) {
+  groupResult.forEach(function(groupTemp) {
+    groupTemp[bookId] = groupTemp[bookId] + 1 || 1;
+  })
+}
+
+module.exports = Classify;
