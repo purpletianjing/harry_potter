8856253bded05f554c5dc69004fe0daccd3d23f7
purple <purpletianjing@163.com>
2015-09-14 16:37:05 +0800

    add an it to spec num

--- spec/spec-model/promotion-spec.js
+++ spec/spec-model/promotion-spec.js
@@ -2,13 +2,16 @@ var Promotion = require("../../model/promotion.js");
 
 describe("Promotion", function() {
   describe("attributes", function() {
-    var promotion = new Promotion('BUY_TOW_DIFFRENCES', 0.05);
+    var promotion = new Promotion('BUY_TWO_DIFFRENCES', 0.05,2);
     it("type", function() {
-      expect(promotion.type).toBe('BUY_TWO_DIFFRsENCES');
+      expect(promotion.type).toBe('BUY_TWO_DIFFRENCES');
     });
 
     it("price",function() {
       expect(promotion.percents).toBe(0.05);
+    });
+    it("num",function() {
+      expect(promotion.num).toBe(2);
     })
   });
 });
