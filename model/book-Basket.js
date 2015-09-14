4a204a7eddd22b08b8a192bc098fac7f2153d886
purple <purpletianjing@163.com>
2015-09-14 16:28:47 +0800

    add a class named BookBasket

--- model/book-Basket.js
+++ model/book-Basket.js
@@ -4,6 +4,14 @@ function BookBasket() {
 
 }
 
-BookBasket.prototype.computePromotions = function(classifyResult) {
-
+BookBasket.prototype.computePromotions = function (classifyResult) {
+  var bookMark = 0;
+  for (var object in classifyResult) {
+    if (classifyResult[object] >= 1) {
+      classifyResult[object]--;
+      bookMark++;
+    }
+  }
 };
+
+module.exports = BookBasket;
