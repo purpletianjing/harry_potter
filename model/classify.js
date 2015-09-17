bef2c38efd58d7d7f819976513d0939021adcdbd
purple <purpletianjing@163.com>
2015-09-17 09:17:32 +0800

    add a function to this file named classifyBookIds

--- model/classify.js
+++ model/classify.js
@@ -3,12 +3,28 @@ function Classify() {
 }
 
 Classify.prototype.group = function(bookIds) {
-  var classifyResult = {};
+  var classifyResult = [];
 
-  bookIds.forEach(function(bookId) {
-    classifyResult[bookId] = classifyResult[bookId] + 1|| 1;
+  bookIds.forEach(function(bookId,index) {
+    classifyBookIds(bookId,index,classifyResult);
   });
+
   return classifyResult;
 };
+
+function classifyBookIds(bookId,index,classifyResult) {
+  var classifyObject = {};
+
+  for (var i = 0; i < classifyResult.length; i++) {
+    if (classifyResult[i].id === bookId) {
+      classifyResult[i].num++;
+      return;
+    }
+  }
+
+  classifyObject.id = bookId;
+  classifyObject.num = 1;
+  classifyResult.push(classifyObject);
+}
 
 module.exports = Classify;
