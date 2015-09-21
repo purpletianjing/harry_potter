d0b6dac2ddf424427c8f066d4c328449fb3c4d71
purple <purpletianjing@163.com>
2015-09-21 15:30:37 +0800

    add a object of Promotion in line 4 where num = 1

--- model/helper/allPromotions.js
+++ model/helper/allPromotions.js
@@ -1,10 +1,12 @@
 var Promotion = require("../promotion.js");
 
 function loadPromotions() {
-  return [new Promotion('BUY_TOW_DIFFRENCES', 0.05, 2),
+  return [new Promotion('BUY_ONE_NO_PROMOTIONS', 0, 1),
+    new Promotion('BUY_TOW_DIFFRENCES', 0.05, 2),
     new Promotion('BUY_THREE_DIFFRENCES', 0.1, 3),
     new Promotion('BUY_FOUR_DIFFENCES', 0.2, 4),
     new Promotion('BUY_FIVE_DIFFENCES', 0.25, 5)];
 }
 
 module.exports = loadPromotions();
+
