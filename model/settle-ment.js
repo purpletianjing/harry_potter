bc31b9bfcd9125566feb9978bbb806dd761a0a6b
purple <purpletianjing@163.com>
2015-09-21 15:34:29 +0800

    add an attribute named bookInventory and three functions named getEveryBookPrice ,getEveryItemPrice and getPromotionPercents and modify the function'name 'getPromotionPrice' to 'getTotalPrice'

--- model/settle-ment.js
+++ model/settle-ment.js
@@ -1,5 +1,5 @@
 function SettleMent() {
-
+  this.bookInventory = [];
 }
 
 var promotions = [];
@@ -13,15 +13,52 @@ SettleMent.getBooks = function(allBooks) {
   books = allBooks;
 }
 
-function getPercents(value,promotions) {
-
-}
-
-SettleMent.prototype.getPromotionPrice = function(count) {
-  var promotionCounts = getPromotionsByCount(count);
-  count.forEach(function(value) {
-    getPercents(value,promotions);
+function getEveryPercent(value,promotions) {
+  promotions.forEach(function(promotion) {
+    if(value.count === promotion.num) {
+      value.percents = promotion.percents;
+    }
   });
 }
+
+SettleMent.prototype.getBookInventory = function(count) {
+  this.bookInventory = count;
+}
+
+SettleMent.prototype.getPromotionPercents = function() {
+  this.bookInventory.forEach(function(value) {
+    getEveryPercent(value,promotions);
+  });
+}
+
+function getEveryBookPrice(totalPrice,bookId,percent) {
+  var price;
+  books.forEach(function(book) {
+    if(bookId === book.id) {
+      price = book.price * (1 - percent);
+    }
+  });
+  return price;
+}
+
+
+function getEveryItemPrice(totalPrice,percent,everyItem) {
+  everyItem.bookIds.forEach(function(bookId) {
+    totalPrice += getEveryBookPrice(totalPrice,bookId,percent);
+  });
+
+  return totalPrice;
+}
+
+SettleMent.prototype.getTotalPrice = function() {
+  var totalPrice = 0;
+  var percent = 0;
+  this.bookInventory.forEach(function(everyItem) {
+    percent = everyItem.percents;
+    totalPrice = getEveryItemPrice(totalPrice,percent,everyItem);
+  });
+  return totalPrice;
+}
+
 
 module.exports = SettleMent;
