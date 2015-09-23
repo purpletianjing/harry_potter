e84b4bed0930786d61a818e7b1e101f2ee285bea
purple <purpletianjing@163.com>
2015-09-23 10:42:15 +0800

    modify spec getTotalPrice

--- spec/spec-model/settle-ment-spec.js
+++ spec/spec-model/settle-ment-spec.js
@@ -1,34 +1,41 @@
 var SettleMent = require("../../model/settle-ment.js");
+var Promotion = require("../../model/promotion.js");
 var allPromotions = require("../../model/helper/allPromotions.js");
-var allBooks = require("../../model/helper/allBooks.js");
-var countResult = [];
+var bookInventory = [];
 var count = [];
-var totalPrice = 59.6;
-
-beforeEach(function () {
-  count = [{count:5 , bookIds: [4, 5, 1, 3, 2]}, {count: 3, bookIds: [1, 3, 2]}, {count: 1, bookIds: [2]}];
-  countResult = [{count: 5, bookIds: [4, 5, 1, 3, 2], percents: 0.25}, {
-    count: 3,
-    bookIds: [1, 3, 2],
-    percents: 0.1
-  }, {count: 1, bookIds: [2], percents: 0}];
-});
+var promotions = [];
+var totalPrice = 108;
 
 describe("SettleMent", function () {
-  describe("function", function () {
+  beforeEach(function () {
+    count = [0, 2, 1, 0, 3, 0];
+    bookInventory = [0, 2, 1, 0, 3, 0];
+    promotions = [new Promotion('BUY_ONE_NO_PROMOTIONS', 0, 1),
+      new Promotion('BUY_TOW_DIFFRENCES', 0.05, 2),
+      new Promotion('BUY_THREE_DIFFRENCES', 0.1, 3),
+      new Promotion('BUY_FOUR_DIFFENCES', 0.2, 4),
+      new Promotion('BUY_FIVE_DIFFENCES', 0.25, 5)];;
+  });
+
+  describe("set attributes", function () {
     var settleMent = new SettleMent();
-    SettleMent.getPromotions(allPromotions);
-    SettleMent.getBooks(allBooks);
-
-    it("getPromotionPercents", function () {
-      settleMent.getBookInventory(count);
-      settleMent.getPromotionPercents();
-
-      expect(settleMent.bookInventory).toEqual(countResult);
+    it("promotions", function () {
+      settleMent.setPromotions(allPromotions);
+      expect(settleMent.promotions).toEqual(promotions);
     });
 
-    it("getTotalPrice",function() {
-      expect(settleMent.getTotalPrice()).toBe(totalPrice);
+    it("bookInventory", function () {
+      settleMent.setBookInventory(count);
+      expect(settleMent.bookInventory).toEqual(bookInventory);
+    });
+  });
+
+  describe("get total price by promotion strategy",function() {
+    var settleMent = new SettleMent();
+    settleMent.setPromotions(allPromotions);
+    it("53 change to 44",function() {
+      settleMent.setBookInventory(count);
+      expect(settleMent.getTotalPrice()).toBe(totalPrice);//totalPrice = 108;
     });
   });
 });
