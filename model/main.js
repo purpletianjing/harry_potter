da4d26a02da0a1e01de828cb12db8240941297f5
purple <purpletianjing@163.com>
2015-09-23 11:52:11 +0800

    add a argument to pos

--- model/main.js
+++ model/main.js
@@ -3,15 +3,15 @@ var BookBasket = require("./book-Basket.js");
 var SettleMent = require("./settle-ment.js");
 
 
-function pos(bookIds) {
+function pos(bookIds,allPromotions) {
   var classify = new Classify();
   var bookBasket = new BookBasket();
   var settleMent = new SettleMent();
 
   var classifyResult = classify.group(bookIds);
   var promotionBooks = bookBasket.computePromotions(classifyResult);
-  settleMent.getBookInventory(promotionBooks);
-  settleMent.getPromotionPercents();
+  settleMent.setPromotions(allPromotions);
+  settleMent.setBookInventory(promotionBooks);
   var totalPrice = settleMent.getTotalPrice();
   return totalPrice;
 }
