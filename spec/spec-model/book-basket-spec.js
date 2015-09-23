9cabae1e36908c5e0786b225c2e729755dfc2f71
purple <purpletianjing@163.com>
2015-09-23 10:44:35 +0800

    change promotions

--- spec/spec-model/book-basket-spec.js
+++ spec/spec-model/book-basket-spec.js
@@ -1,11 +1,11 @@
 var BookBasket = require("../../model/book-Basket.js");
 
-describe("BookBasket",function() {
-  describe("function",function() {
-    var classifyResult = [{id:1,num:2},{id:2,num:3},{id:3,num:2},{id:4,num:1},{id:5,num:1}];
-    var promotions = [{ count: 5, bookIds: [ 4, 5, 1, 3, 2 ] },{ count: 3, bookIds: [ 1, 3, 2 ] },{ count: 1, bookIds: [ 2 ] }];
+describe("BookBasket", function () {
+  describe("function", function () {
+    var classifyResult = [{id: 1, num: 6}, {id: 2, num: 4}, {id: 3, num: 3}, {id: 4, num: 2}, {id: 5, num: 1}];
+    var promotions = [ 0, 2, 1, 1, 1, 1 ];
     var bookBasket = new BookBasket();
-    it("computePromotions",function() {
+    it("computePromotions", function () {
       expect(bookBasket.computePromotions(classifyResult)).toEqual(promotions);
     });
   });
