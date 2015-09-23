53897fa06e141e445c9f8ff19208b06e35b9c20c
purple <purpletianjing@163.com>
2015-09-23 10:45:29 +0800

    add spec-data to bookIds

--- spec/spec-model/classify-spec.js
+++ spec/spec-model/classify-spec.js
@@ -3,12 +3,12 @@ var Classify = require("../../model/classify.js");
 var bookIds;
 
 beforeEach(function() {
-  bookIds = [1,2,3,1,2,4,5,3,2];
+  bookIds = [1,2,3,1,2,4,5,3,2,1,1,1,2,3,4,1];
 });
 
 describe("Classify",function() {
   var classify = new Classify();
-  var classifyResult = [{id:1,num:2},{id:2,num:3},{id:3,num:2},{id:4,num:1},{id:5,num:1}];
+  var classifyResult = [ { id: 1, num: 5 },{ id: 2, num: 4 },{ id: 3, num: 3 },{ id: 4, num: 2 },{ id: 5, num: 1 } ];
   describe("function",function() {
     it("group",function() {
       expect(classify.group(bookIds)).toEqual(classifyResult);
